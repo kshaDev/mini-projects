438643d264acb47fd6e19b9fbece094975a21af1
Kshama Sharma <sharmakshama2806@gmail.com>
2025-10-15 12:33:38 +0530

    Delete mini proj/script.js

--- mini proj/script.js
+++
@@ -1,9 +0,0 @@
-// let id = document.getElementsByTagName("div");
-// console.dir(id);
-// console.log(id);
-
-// let elements = document.querySelectorAll(".form_control");
-// console.dir(elements);
-// let elements = document.querySelector("div");
-// console.dir(elements);
-// console.dir(document.body.firstChild);
